ab65e4d031cb2d1472ec4d352e40d171aafca88d
Yared29 <yaredtekile5@gmail.com>
2023-06-23 22:30:08 +0300

    (add) gitignore, database connection

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import colors from "colors";
+import morgan from "morgan";
+
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("AgProPool API is running...");
+});
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
+  )
+);
